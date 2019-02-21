a8050c94b4d32fa5bb4302a3a01ca6c1bbde9131
Jonathan Sexton <joncsexton@.com>
2019-02-21 15:40:57 -0600

    moved articles to top level component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,50 @@ import Header from "./components/layout/Header";
 import NewsList from "./components/NewsList";
 
 class App extends Component {
+  state = {
+    articles: [
+      {
+        id: 0,
+        title: "News Story One",
+        read: false,
+      },
+      {
+        id: 1,
+        title: "News Story One",
+        read: false,
+      },
+      {
+        id: 2,
+        title: "News Story One",
+        read: false,
+      },
+      {
+        id: 3,
+        title: "News Story One",
+        read: false,
+      },
+      {
+        id: 4,
+        title: "News Story One",
+        read: false,
+      },
+      {
+        id: 5,
+        title: "News Story One",
+        read: false,
+      },
+      {
+        id: 6,
+        title: "News Story One",
+        read: false,
+      },
+      {
+        id: 7,
+        title: "News Story One",
+        read: false,
+      },
+    ],
+  };
   render() {
     return (
       <section className="App">
--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import NewsItem from './NewsItem';
+import React from "react";
+import NewsItem from "./NewsItem";
 
 class NewsList extends React.Component {
-    render() {
-        return (
-            <section id="news-list-container">
-                <h1>
-                    This is the news stories container Component
-                </h1>
-                <NewsItem />
-            </section>
-        )
-    }
+  render() {
+    return (
+      <section id="news-list-container">
+        <h1>This is the news stories container Component</h1>
+        <NewsItem />
+      </section>
+    );
+  }
 }
 
 export default NewsList;
