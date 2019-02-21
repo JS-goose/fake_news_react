ab545ef9b01f91f55b2a2a3da320922e423fe4e5
Jonathan Sexton <joncsexton@gmail.com>
2019-02-21 05:06:24 -0600

    moved header content into header file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/layout/Header';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Work in progress</h1>
-      </div>
+      <Header />
     );
   }
 }
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export default function Header() {
   return (
     <div>
-      
+      <h1>Work in progress</h1>
     </div>
-  )
+  );
 }
