6f0e36b581101237a5efcdbb3c445e624aeb32d0
Jonathan Sexton <joncsexton@.com>
2019-02-21 16:15:07 -0600

    added place holder text for articles

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,10 +2,15 @@ import React from "react";
 
 class NewsItem extends React.Component {
   render() {
-      const {id, title} = this.props.articles;
+    const { id, title } = this.props.articles;
     return (
       <article>
-        <p>{title}</p>
+        <h1>{title}</h1>
+        <p>
+          Here's some sample text from the article Here's some sample text from the article Here's
+          some sample text from the article Here's some sample text from the article Here's some
+          sample text from the article Here's some sample text from the article
+        </p>
       </article>
     );
   }
