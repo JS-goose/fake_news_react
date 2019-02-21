30355e3c4ede93971598a76fc7dfd60c23ab3c31
Jonathan Sexton <joncsexton@.com>
2019-02-21 15:05:19 -0600

    created NewsList component and added basic structure

---
+++ src/components/NewsList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+Class NewsList extends React.Component {
+    constructor (props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <section>
+                <h1>
+                    This is the news stories container Component
+                </h1>
+            </section>
+        )
+    }
+}
+
+export default NewsList;
