c548f604c8170a4a9462d4749907fd2bbf096d1e
Jonathan Sexton <joncsexton@gmail.com>
2019-02-25 05:20:17 -0600

    gave article titles a className

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,7 +5,7 @@ class NewsItem extends React.Component {
     const { id, title } = this.props.articles;
     return (
       <article className="article-list-stories" id={id}>
-        <h1>{title}</h1>
+        <h1 className="article-titles">{title}</h1>
         <p>
           Here's some sample text from the article Here's some sample text from the article Here's
           some sample text from the article Here's some sample text from the article Here's some
