0506bba77920e9f49c63d113c2d1d75e3239061c
Jonathan Sexton <joncsexton@gmail.com>
2019-02-27 04:33:04 -0600

    imported axios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import axios from 'axios';
 import "./App.css";
 import Header from "./components/layout/Header";
 import NewsList from "./components/NewsList";
@@ -64,7 +65,7 @@ class App extends Component {
               </section>
             )}
           />
-        <Route path="/about" component={About} />
+          <Route path="/about" component={About} />
         </section>
       </Router>
     );
