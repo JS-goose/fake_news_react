3f41e2e55008687f1e4e7d215cb3b2fe32e0585c
Jonathan Sexton <joncsexton@gmail.com>
2019-02-27 18:28:41 -0600

    added propTypes import statement

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, PropTypes } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import axios from "axios";
 import "./App.css";
