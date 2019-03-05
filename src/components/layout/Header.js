b0b8dd5412489c576233cb3d05fcb2db2e961a3b
Jonathan Sexton <joncsexton@gmail.com>
2019-03-04 18:28:38 -0600

    added the header section

---
+++ src/components/layout/Header.js
@@ -0,0 +1,10 @@
+import React, { Component, PropTypes } from "react";
+import { Link } from "react-router-dom";
+
+class Header extends Component {
+   render() {
+       return(
+           <p>This is the header section</p>
+       )
+   }
+}
