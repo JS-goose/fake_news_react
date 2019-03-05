b6c0f325ea0d34b4ec34518431acf1e24a041f0a
Jonathan Sexton <joncsexton@gmail.com>
2019-03-04 18:29:03 -0600

    added export to Header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,9 +2,9 @@ import React, { Component, PropTypes } from "react";
 import { Link } from "react-router-dom";
 
 class Header extends Component {
-   render() {
-       return(
-           <p>This is the header section</p>
-       )
-   }
+  render() {
+    return <p>This is the header section</p>;
+  }
 }
+
+export default Header;
