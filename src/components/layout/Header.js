ce0fc83f76c51bd997b506ab82c16ba771d149eb
Jonathan Sexton <joncsexton@gmail.com>
2019-03-05 05:12:52 -0600

    added constructor to play around with for learning purposes

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,10 +2,19 @@ import React, { Component, PropTypes } from "react";
 import { Link } from "react-router-dom";
 
 class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { name: 'Header'}
+    }
+
+    handleClick = () => {
+        console.log(this);
+    }
+
   render() {
     return (
       <section id="header-container">
-        <p>This is the header section</p>
+        <p>This is the <button onClick={this.handleClick}>{this.state.name}</button> section</p>
       </section>
     );
   }
