3002fee11b7b508e4862dd9b18453330e17285c2
Jonathan Sexton <joncsexton@.com>
2019-03-05 15:57:44 -0600

    added articles from state to recent articles section

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,19 +2,12 @@ import React, { Component, PropTypes } from "react";
 import { Link } from "react-router-dom";
 
 class Header extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { name: 'Header'}
-    }
 
-    handleClick = () => {
-        console.log(this);
-    }
 
   render() {
     return (
       <section id="header-container">
-        <p>This is the <button onClick={this.handleClick}>{this.state.name}</button> section</p>
+        <p>This is the Header section</p>
       </section>
     );
   }
--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,64 +1,65 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function Header() {
-  return (
-    <header articles={this.props.articles}>
-      <nav id="navbar">
-        <h1>Fake News Site</h1>
-        <ul id="nav-links-list">
-          <li className="sidebar-title">
-            <p>Sidebar Navigation</p>
-          </li>
-          <li className="nav-links">
-            <Link to="/">
-              <button>Home</button>
-            </Link>
-          </li>
-          <li className="nav-links">
-            <Link to="/about">
-              <button>About</button>
-            </Link>
-          </li>
-          <li className="nav-links">
-            <Link to="/about">
-              <button>All categories</button>
-            </Link>
-          </li>
-          <li className="nav-links">
-            <Link to="/about">
-              <button>Fake News</button>
-            </Link>
-          </li>
-          <li className="nav-links">
-            <Link to="/about">
-              <button>Conspiracy</button>
-            </Link>
-          </li>
-          <li className="nav-links">
-            <Link to="/about">
-              <button>Click Bait</button>
-            </Link>
-          </li>
-          <li className="nav-links">
-            <a href="#login">Log In</a>
-          </li>
-        </ul>
-      </nav>
-      <section id="latest-stories">
-        <ul>
-          <li className="sidebar-title">
-            <p>Latest Stories</p>
-          </li>
-          <li>News story one</li>
-          <li>News story one</li>
-          <li>News story one</li>
-          <li>News story one</li>
-          <li>News story one</li>
-          <li>News story one</li>
-          <li>News story one</li>
-        </ul>
-      </section>
-    </header>
-  );
+class Sidebar extends React.Component {
+  render() {
+    return (
+      <header articles={this.props.articles}>
+        <nav id="navbar">
+          <h1>Fake News Site</h1>
+          <ul id="nav-links-list">
+            <li className="sidebar-title">
+              <p>Sidebar Navigation</p>
+            </li>
+            <li className="nav-links">
+              <Link to="/">
+                <button>Home</button>
+              </Link>
+            </li>
+            <li className="nav-links">
+              <Link to="/about">
+                <button>About</button>
+              </Link>
+            </li>
+            <li className="nav-links">
+              <Link to="/about">
+                <button>All categories</button>
+              </Link>
+            </li>
+            <li className="nav-links">
+              <Link to="/about">
+                <button>Fake News</button>
+              </Link>
+            </li>
+            <li className="nav-links">
+              <Link to="/about">
+                <button>Conspiracy</button>
+              </Link>
+            </li>
+            <li className="nav-links">
+              <Link to="/about">
+                <button>Click Bait</button>
+              </Link>
+            </li>
+            <li className="nav-links">
+              <a href="#login">Log In</a>
+            </li>
+          </ul>
+        </nav>
+        <section id="latest-stories">
+          <ul>
+            <li className="sidebar-title">
+              <p>Latest Stories</p>
+            </li>
+            <li>{this.props.articles.map((item) => {
+              console.log(item)
+              return <li><a href={item.url} target="_blank">{item.title}</a></li>
+            })}</li>
+          </ul>
+        </section>
+      </header>
+    );
+  }
 }
+
+export default Sidebar;
