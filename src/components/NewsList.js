10448781c6e2ab7c8fd550b9a7a47e4c30061e09
Jonathan Sexton <joncsexton@gmail.com>
2019-03-07 08:10:09 -0600

    changed banner description to a div and updated styling

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -5,14 +5,16 @@ class NewsList extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <p id="banner-description">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-          sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-          est laborum.
-        </p>
+        <div id="banner-description">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
+            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+            mollit anim id est laborum.
+          </p>
+        </div>
         <section id="news-list-container-inner">
           {this.props.articles.map((article) => (
             <NewsItem articles={article} key={article.id} />
