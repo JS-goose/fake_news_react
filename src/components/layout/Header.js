ba183034cd94224eb48f0ae22900cc5ee438c5b9
Jonathan Sexton <joncsexton@gmail.com>
2019-03-14 05:47:11 -0500

    added state to header to capture name/pass (only for practice)

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,7 +2,10 @@ import React, { Component, PropTypes } from "react";
 import { Link } from "react-router-dom";
 
 class Header extends Component {
-
+  state = {
+    userName: '',
+    password: ''
+  }
 
   render() {
     return (
