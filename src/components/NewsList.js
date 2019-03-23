e89ac8d8f8890eda24859abfb90116b35de88072
Jonathan Sexton <joncsexton@.com>
2019-03-23 12:37:21 -0500

    Added a section title to news list container

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -29,6 +29,7 @@ class NewsList extends React.Component {
             mollit anim id est laborum.
           </p>
         </div>
+        <h1>Hot Stories of the Day</h1>
         <section id="news-list-container-inner">
           {this.props.articles.map((article) => (
             <NewsItem articles={article} key={article.id} />
